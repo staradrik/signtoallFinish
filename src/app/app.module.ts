489854556552709dc427a0862dbe1c81c513eccd
staradrik <adrianayara12@gmail.com>
2022-02-24 14:50:46 -0500

    comienzo nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import {CascadeSelectModule} from 'primeng/cascadeselect';
+import {CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputTextModule} from 'primeng/inputtext';
+import { NgModule } from '@angular/core';
+import {PasswordModule} from 'primeng/password';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    CascadeSelectModule,
+    CheckboxModule,
+    DropdownModule,
+    InputTextModule,
+    PasswordModule,
+    RadioButtonModule,
+    ReactiveFormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
