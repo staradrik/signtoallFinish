5ee37486432b7540a2c614ec82bfee59eb56da76
staradrik <adrianayara12@gmail.com>
2022-02-28 20:00:34 -0500

    dataView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 //Modulos
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
 import {CascadeSelectModule} from 'primeng/cascadeselect';
 import {CheckboxModule} from 'primeng/checkbox';
+import {DataViewModule} from 'primeng/dataview';
+import {DialogModule} from 'primeng/dialog';
 import {DropdownModule} from 'primeng/dropdown';
 import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import {InputTextModule} from 'primeng/inputtext';
 import { NgModule } from '@angular/core';
+import {PanelModule} from 'primeng/panel';
 import {PasswordModule} from 'primeng/password';
 import {RadioButtonModule} from 'primeng/radiobutton';
+import {RatingModule} from 'primeng/rating';
 import { ReactiveFormsModule } from '@angular/forms';
+import {RippleModule} from 'primeng/ripple';
 import {SelectButtonModule} from 'primeng/selectbutton';
+import {ToastModule} from 'primeng/toast';
 
+//Servicios
+import { ServicioService } from './servicios/servicio.service';
 //Componentes
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -20,6 +31,15 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ModuloMatematicasComponent } from './modulo-matematicas/modulo-matematicas.component';
 import { ModuloEspannolComponent } from './modulo-espannol/modulo-espannol.component';
+
+
+
+
+
+
+
+
+
 
 
 @NgModule({
@@ -34,18 +54,27 @@ import { ModuloEspannolComponent } from './modulo-espannol/modulo-espannol.compo
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     ButtonModule,
+    CardModule,
     CascadeSelectModule,
     CheckboxModule,
+    DataViewModule,
+    DialogModule,
     DropdownModule,
     FormsModule,
+    HttpClientModule ,
     InputTextModule,
+    PanelModule,
     PasswordModule,
     RadioButtonModule,
+    RatingModule,
+    RippleModule,
     ReactiveFormsModule,
-    SelectButtonModule 
+    SelectButtonModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [ServicioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modulo-matematicas/modulo-matematicas.component.ts
+++ src/app/modulo-matematicas/modulo-matematicas.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+//import { ProductService } from './productservice';
+import { Product } from '../interfaces/product';
+import {SelectItem} from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { ServicioService } from '../servicios/servicio.service';
 
 @Component({
   selector: 'app-modulo-matematicas',
   templateUrl: './modulo-matematicas.component.html',
-  styles: [
-  ]
+  styleUrls: [] 
 })
-export class ModuloMatematicasComponent implements OnInit {
+export class ModuloMatematicasComponent {
 
-  constructor() { }
+  products: Product[]=[];
 
-  ngOnInit(): void {
+  sortOptions: SelectItem[] =[];
+
+  sortOrder: number = 0;
+
+  sortField: string = "";
+
+  constructor(private productService: ServicioService, private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+      this.productService.getProducts().then(data => this.products = data);
+
+      this.sortOptions = [
+          {label: 'Reciente', value: '!price'},
+          {label: 'Antigua', value: 'price'}
+      ];
+
+      this.primengConfig.ripple = true;
   }
+  
+  onSortChange(event: any) {
+      let value = event.value;
 
+      if (value.indexOf('!') === 0) {
+          this.sortOrder = -1;
+          this.sortField = value.substring(1, value.length);
+      }
+      else {
+          this.sortOrder = 1;
+          this.sortField = value;
+      }
+  }
 }
