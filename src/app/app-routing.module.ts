05f6cc8d31726728c6c28457c4300397d62c3121
Bing1177 <cesarleonardo88@hotmail.com>
2022-02-28 22:37:40 -0500

    Rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { MemoramaComponent } from './modulo-espannol/memorama/memorama.component';
+import { ModuloEspannolComponent } from './modulo-espannol/modulo-espannol.component';
+import { ModuloMatematicasComponent } from './modulo-matematicas/modulo-matematicas.component';
+import { SopaNumerosComponent } from './modulo-matematicas/sopa-numeros/sopa-numeros.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:'',
+    children: [
+      {path:'login', component:LoginComponent},
+      {path:'registro', component:RegisterComponent},
+      {path:'modEspannol',component:ModuloEspannolComponent,
+        children:[ 
+        {path:'memorama', component:MemoramaComponent} ]
+      },
+      {path:'modMatematicas',
+      component:ModuloMatematicasComponent,
+        children:[
+        {path:'sopNumeros', component:SopaNumerosComponent} ]
+      },
+      { path: '**', redirectTo: '/notfound' }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
