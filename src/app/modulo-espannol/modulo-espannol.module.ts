09fc817a837ba9a9214c99639198c806347b290d
Bing1177 <cesarleonardo88@hotmail.com>
2022-03-02 13:44:50 -0500

    Lazy Load parte 2

---
+++ src/app/modulo-espannol/modulo-espannol.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuloEspannolRoutingModule } from './modulo-espannol-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModuloEspannolRoutingModule
+  ]
+})
+export class ModuloEspannolModule { }
---
+++ src/app/modulo-matematicas/modulo-matematicas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuloMatematicasRoutingModule } from './modulo-matematicas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModuloMatematicasRoutingModule
+  ]
+})
+export class ModuloMatematicasModule { }
