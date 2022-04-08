b0fd5780993b2234f36cfe3b87a88218c763ebc0
Niyireth Cortes <niyirethcortes@periferia-it.com>
2022-04-08 14:01:13 -0500

    Dependencia sin uso

--- src/app/website/pages/matematicas/sudoku/grid/grid.component.ts
+++ src/app/website/pages/matematicas/sudoku/grid/grid.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MatematicasService } from 'src/app/services/matematicas.service';
 
