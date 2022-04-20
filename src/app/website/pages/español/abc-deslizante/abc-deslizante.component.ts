bd5d14f3c16540996e2fb00845272ee280c69e28
Bing1177 <cesarleonardo88@hotmail.com>
2022-04-19 21:31:44 -0500

    componente abecedario correctamente

--- src/app/website/pages/español/abc-deslizante/abc-deslizante.component.ts
+++ src/app/website/pages/español/abc-deslizante/abc-deslizante.component.ts
@@ -3,22 +3,23 @@ import { RutaBreadcrumService } from 'src/app/services/ruta-breadcrum.service';
 import { EspannolService } from '../../../../services/espannol.service';
 
 @Component({
-  selector: 'app-vocabulario',
-  templateUrl: './vocabulario.component.html',
-  styleUrls: ['./vocabulario.component.scss']
+  selector: 'app-abc-deslizante',
+  templateUrl: './abc-deslizante.component.html',
+  styleUrls: ['./abc-deslizante.component.scss']
 })
-export class VocabularioComponent implements OnInit {
+export class AbcDeslizanteComponent implements OnInit {
 
   constructor(
     public boardGame: EspannolService,
     private breadcrumbService: RutaBreadcrumService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.breadcrumbService.setItems([
       { label: 'Actividades', routerLink: ['/actividades']}, 
-      { label: 'vocabulario' }
+      { label: 'Abecedario deslizante' }
     ]);
     this.boardGame.initGame();
   }
+
 }
