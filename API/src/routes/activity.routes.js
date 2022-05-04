03c86d2ffc077093c4254e82db1283e4081e91fc
Danilo <danilovaronsilva@gmail.com>
2022-05-04 10:20:04 -0500

    Se añadio otra ruta para las actividades

--- API/src/routes/activity.routes.js
+++ API/src/routes/activity.routes.js
@@ -5,46 +5,61 @@ const jwt = require("jsonwebtoken");
 
 const { jwtSecret } = require("../keys");
 
-router.put("/actualizarActividad/:id_estudiante/:id_actividad", async (req, res) => {
-  const { id_estudiante, id_actividad } = req.params;
+router.put(
+  "/actualizarActividad/:id_estudiante/:id_actividad",
+  async (req, res) => {
+    const { id_estudiante, id_actividad } = req.params;
 
-  // if (!req.headers.authorization) {
-  //   return res.status(401).send("No tiene autorización");
-  // }
+    // if (!req.headers.authorization) {
+    //   return res.status(401).send("No tiene autorización");
+    // }
 
-  // const token = req.headers.authorization.split(" ")[1];
+    // const token = req.headers.authorization.split(" ")[1];
 
-  // if (token === null) {
-  //   return res.status(401).send("No tiene autorización");
-  // }
+    // if (token === null) {
+    //   return res.status(401).send("No tiene autorización");
+    // }
 
-  // const payload = jwt.verify(token, jwtSecret);
+    // const payload = jwt.verify(token, jwtSecret);
 
-  await pool.query(
-    `UPDATE estudiante_actividad SET actividad_realizada = "${req.body.actividad_realizada}", nota_actividad = "${req.body.nota}" WHERE id_estudiante = ${id_estudiante} AND id_actividad = ${id_actividad}`
-  );
+    await pool.query(
+      `UPDATE estudiante_actividad SET actividad_realizada = "${req.body.actividad_realizada}", nota_actividad = "${req.body.nota}" WHERE id_estudiante = ${id_estudiante} AND id_actividad = ${id_actividad}`
+    );
 
-  res.json({ message: "Actividad realizada" });
-  
-});
+    res.json({ message: "Actividad realizada" });
+  }
+);
 
 router.get("/verActividad/:id_estudiante/:id_actividad", async (req, res) => {
   const { id_estudiante, id_actividad } = req.params;
 
-  const rows = await pool.query(`SELECT estudiante.nombres, estudiante.apellidos, actividad.nombre_actividad, estudiante_actividad.nota_actividad FROM actividad, estudiante_actividad, estudiante WHERE estudiante_actividad.id_actividad = actividad.id_actividad AND estudiante_actividad.id_estudiante = estudiante.id_estudiante AND actividad.id_actividad = ${id_actividad} AND estudiante.id_estudiante = ${id_estudiante} AND estudiante_actividad.nota_actividad = 5`);
+  const rows = await pool.query(
+    `SELECT estudiante.nombres, estudiante.apellidos, actividad.nombre_actividad, estudiante_actividad.nota_actividad FROM actividad, estudiante_actividad, estudiante WHERE estudiante_actividad.id_actividad = actividad.id_actividad AND estudiante_actividad.id_estudiante = estudiante.id_estudiante AND actividad.id_actividad = ${id_actividad} AND estudiante.id_estudiante = ${id_estudiante} AND estudiante_actividad.nota_actividad = 5`
+  );
 
   const activity = rows[0];
 
-  console.log(activity[0]);
-
   if (activity[0] === undefined) {
     res.json({ message: "El estudiante no ha realizado la actividad" });
-    
-  }else{
+  } else {
     res.json(activity);
-
   }
+});
 
+router.get("/Actividades/:id_estudiante", async (req, res) => {
+  const { id_estudiante } = req.params;
+
+  const rows = await pool.query(
+    `SELECT estudiante.nombres, estudiante.apellidos, actividad.nombre_actividad, estudiante_actividad.nota_actividad FROM actividad, estudiante_actividad, estudiante WHERE estudiante_actividad.id_actividad = actividad.id_actividad AND estudiante_actividad.id_estudiante = estudiante.id_estudiante AND estudiante.id_estudiante = ${id_estudiante}`
+  );
+
+  const activity = rows[0];
+
+  if (activity[0] === undefined) {
+    res.json({ message: "El estudiante no tiene registrado actividades" });
+  } else {
+    res.json(activity);
+  }
 });
 
 module.exports = router;
